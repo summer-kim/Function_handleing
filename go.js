f8acb187ebbd2a2a410e0023d83a482d6bec4ee5
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-04-15 22:53:51 +0900

    go function to express precisely

--- go.js
+++ go.js
@@ -16,3 +16,13 @@ log(
     )
   )
 );
+const go = (...args) => reduce((a, f) => f(a), args);
+
+go(
+  0,
+  (a) => a + 1,
+  (a) => a + 10,
+  (a) => a + 100,
+  log
+);
+const add = (a, b) => a + b;
